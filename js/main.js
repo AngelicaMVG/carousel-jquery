da58632db65129d8e4fdb2c2587e543d1bd4f12f
AngelicaMVG <angie.velazquez2211@gmail.com>
2017-05-29 16:47:16 -0500

    ahora funcionan los botones

--- js/main.js
+++ js/main.js
@@ -1,19 +1,48 @@
-$(function(){
-  $('#btn1').click(function(){
-    $('#btn1').css('background-color', 'red');
-    $('img1').show();
-});
+//MUESTRA LAS IMAGENES DEPENDIENDO DEL BOTON//
+$(document).ready( function(){
+  $('.img').hide();
+  $('#img1').show();
+  $('.btns').css('background-color', 'rgb(23, 105, 97)');
 });
 
-$(function(){
-    $('#slider div:gt(0)').hide();
-    setInterval(function(){
-      $('#slider div:first-child').fadeOut(0)
-         .next('div').fadeIn(1000)
-         .end().appendTo('#slider');}, 3000);
-    // $('.controles span:gt(0)').css("background-color", 'white');
-    // setInterval(function(){
-    //  $('.controles span:first-child').css("background-color", "rgb(205, 139, 138)")
-    //       .next('span').fadeIn(1000)
-    //       .end().appendTo('.controles');}, 3000);
+$('#btn1').click(function(){
+  $('.img').hide();
+  $('#img1').show();
+  $('.btns').css('background-color', 'rgb(23, 105, 97)');
+  $('#btn1').css('background-color', '#fff');
 });
+
+$('#btn2').click(function(){
+  $('.img').hide();
+  $('#img2').show();
+  $('.btns').css('background-color', 'rgb(23, 105, 97)');
+  $('#btn2').css('background-color', '#fff');
+});
+
+$('#btn3').click(function(){
+  $('.img').hide();
+  $('#img3').show();
+  $('.btns').css('background-color', 'rgb(23, 105, 97)');
+  $('#btn3').css('background-color', '#fff');
+});
+
+$('#btn4').click(function(){
+  $('.img').hide();
+  $('#img4').show();
+  $('.btns').css('background-color', 'rgb(23, 105, 97)');
+  $('#btn4').css('background-color', '#fff');
+});
+
+//MUESTRA LAS IMAGENES CADA 3 SEGUNDOS//
+// $(function(){
+//     $('#slider div:gt(0)').hide();
+//     setInterval(function(){
+//       $('#slider div:first-child').fadeOut(0)
+//          .next('div').fadeIn(1000)
+//          .end().appendTo('#slider');}, 3000);
+//     $('.controles span:gt(0)').css("background-color", 'white');
+//     setInterval(function(){
+//      $('.controles span:first-child').css("background-color", "rgb(205, 139, 138)");
+//           .next('span').fadeIn(1000)
+//           .end().appendTo('.controles');}, 3000);
+// });
